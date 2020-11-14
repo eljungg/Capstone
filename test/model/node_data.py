5d646b385321ed942eacf68bf6994f6a4c1fe8bc
luke <llschulz@asu.edu>
2020-11-14 15:36:30 -0700

    added data node type checking

--- test/model/node_data.py
+++ test/model/node_data.py
@@ -7,6 +7,8 @@ class NodeData:
     def __init__(self): ##simple params for our object
         print("Node Data Model has been created!")
         self.nodeType = None # will contain opcode that defines type
+        self.val = None # Simpler than .vals List, Didnt want to delete vals[] if anyone was using it.mro
+        self.valType = None #Simpler than .valsType List, didnt want to delete ValsType[] if anyoe was using it
         self.vals = [] # list, can handle multiple data vals
         self.valsTypes = [] # matching list to vals, holds type infor for each val. e.g TYPE_STRING, TYPE_INT
         self.name = "Name me"
--- test/nodes/data_node.py
+++ test/nodes/data_node.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import *
 from nodeeditor.utils import dumpException
 from vpl_node import * # get our custom node base
+from Capstone.test.conf import *
 
 class DataContent(QDMNodeContentWidget):
     def initUI(self):
@@ -16,3 +17,51 @@ class DataNode(VplNode):
         self.grNode = VplGraphicsNode(self)
         #below is onTextChanged event for simple self.edit Label
         self.content.edit.textChanged.connect(self.onInputChanged)
+        self.content.edit.textChanged.connect(self.determineDataType)
+    
+    def determineDataType(self):
+        ### Determine the type of data given in Text Box by user ###
+        self.data.val = (self.content.edit.text())
+        val = self.data.val
+        if self.__isInt(val) == True:
+            self.data.valType = TYPE_INT
+            return
+        elif self.__isFloat(val) == True:
+            self.data.valType = TYPE_DOUBLE
+            return
+        elif self.__isBool(val) == True:
+            self.data.valType = TYPE_BOOL
+            return
+        elif self.__isChar(val) == True:
+            self.data.valType = TYPE_CHAR
+            return
+        else:
+            self.data.valType = TYPE_STRING
+            return
+
+            
+
+
+    def __isInt(self , val): #helper function for determineType
+        try:
+            int(val)
+            return True
+        except ValueError:
+            return False
+    def __isFloat(self, val):
+        try:
+            float(val)
+            return True
+        except ValueError:
+            return False
+    def __isBool(self, val):
+        lcVal = val.lower()
+        if lcVal == "false" or lcVal == "true":
+            return True
+        else:
+            return False
+    def __isChar(self, val): #Python doesnt do Char, but VIPLE does so we just emulate?
+        if len(val) == 1:
+            return True
+        else:
+            return False
