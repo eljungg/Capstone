57e4bdb87a08bac09d1b417084782f374736ab93
eljungg <54684889+eljungg@users.noreply.github.com>
2021-01-28 07:54:23 -0700

    Delete test directory

--- test/model/variables.py
+++
@@ -1,9 +0,0 @@
-
-from Capstone.test.conf import * 
-class VariablesData:
-    """This is a class which will hold the variables for an entire subWindow (instance of VPL)"""
-    ###
-    def __init__(self): ##simple params for our object
-        print("Node Variable Model has been created!")
-        self.variables = [] # simple list to hold our variables (for now)
-    
