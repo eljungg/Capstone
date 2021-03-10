539f2523120c63313a58a8ce285aa5cb9baeb9b6
Kent Larson <kmlarso6@asu.edu>
2021-03-10 01:28:12 -0700

    Added dynamic port add. Still working on sub.

--- test/nodes/if_node.py
+++ test/nodes/if_node.py
@@ -9,15 +9,20 @@ from nodeeditor.node_node import *
 
 import re
 
-TotalIfs = 1
-
 class IfNodeContent(QDMNodeContentWidget):
+    TotalIfs = 1
+
     def __init__(self, parent, variablesRef):
         self.vars = variablesRef
         super().__init__(parent)
 
     def initUI(self):
         self.layout = QGridLayout()
+        self.backup = QGridLayout()
+
+        self.layout.setRowStretch(1, 3)
+        self.layout.setRowStretch(2, 3)
+        
 
         self.comboBox = []
         self.comboBox.append(QComboBox(self))
@@ -45,96 +50,18 @@ class IfNodeContent(QDMNodeContentWidget):
         self.layout.addWidget(self.addBtn, 2, 1)
         self.setLayout(self.layout)
         self.redrawComboBox()
-        self.registerButtons()
 
     def redrawComboBox(self): # function displays new variables in dropdown. (GUI REFRESH)
-        global TotalIfs
-        self.comboBox[TotalIfs - 1].clear()
-        self.comboBox[TotalIfs - 1].addItems(['true', 'false'])
+        self.comboBox[self.TotalIfs - 1].clear()
+        self.comboBox[self.TotalIfs - 1].addItems(['true', 'false'])
         for var in self.vars.variables:
-            self.comboBox[TotalIfs - 1].addItem(var.name)
-        self.comboBox[TotalIfs - 1].addItem('value')
+            self.comboBox[self.TotalIfs - 1].addItem(var.name)
+        self.comboBox[self.TotalIfs - 1].addItem('value')
 
     def setContentVariables(self, variables):
         self.vars = variables
 
-    def registerButtons(self):
-        self.subBtn.clicked.connect(self.decreaseWidgetSize)
-        self.addBtn.clicked.connect(self.increaseWidgetSize)
-
-    def increaseWidgetSize(self):
-        global TotalIfs
-        TotalIfs = TotalIfs + 1
-
-        self.layout.removeWidget(self.subBtn)
-        self.subBtn.deleteLater()
-        self.subBtn = None
-        self.layout.removeWidget(self.elseLbl)
-        self.elseLbl.deleteLater()
-        self.elseLbl = None
-        self.layout.removeWidget(self.addBtn)
-        self.addBtn.deleteLater()
-        self.addBtn = None      
-
-        self.comboBox.append(QComboBox(self))
-        self.comboBox[TotalIfs - 1].addItems(self.initialList)
-
-        self.edit.append(QLineEdit('' , self))
-        self.comboBox[TotalIfs - 1].setLineEdit(self.edit[TotalIfs - 1])
-
-        self.edit[TotalIfs - 1].setAlignment(Qt.AlignCenter)
         
-        self.subBtn = QPushButton('-', self)
-        self.addBtn = QPushButton('+', self)
-        self.elseLbl = QLabel('Else', self)
-        self.elseLbl.setAlignment(Qt.AlignCenter)
-        
-        self.layout.addWidget(self.comboBox[TotalIfs - 1], TotalIfs, 1, 1, 3)        
-        
-        self.layout.addWidget(self.subBtn, TotalIfs + 1, 2)
-        self.layout.addWidget(self.elseLbl, TotalIfs + 1, 3)
-        self.layout.addWidget(self.addBtn, TotalIfs + 1, 1)
-        
-        self.setLayout(self.layout)
-        self.redrawComboBox()
-        self.registerButtons()
-
-    def decreaseWidgetSize(self):
-        global TotalIfs
-
-        if (TotalIfs > 1):
-            self.layout.removeWidget(self.subBtn)
-            self.subBtn.deleteLater()
-            self.subBtn = None
-            self.layout.removeWidget(self.elseLbl)
-            self.elseLbl.deleteLater()
-            self.elseLbl = None
-            self.layout.removeWidget(self.addBtn)
-            self.addBtn.deleteLater()
-            self.addBtn = None   
-            
-            self.layout.removeWidget(self.comboBox[TotalIfs - 1])
-            self.comboBox[TotalIfs - 1].setParent(None) 
-            self.comboBox.pop()
-            self.edit.pop()
-
-            TotalIfs = TotalIfs - 1
-            
-            self.subBtn = QPushButton('-', self)
-            self.addBtn = QPushButton('+', self)
-            self.elseLbl = QLabel('Else', self)
-            self.elseLbl.setAlignment(Qt.AlignCenter)
-            
-            self.layout.addWidget(self.subBtn, TotalIfs + 1, 2)
-            self.layout.addWidget(self.elseLbl, TotalIfs + 1, 3)
-            self.layout.addWidget(self.addBtn, TotalIfs + 1, 1)
-            
-            self.setLayout(self.layout)
-
-            self.registerButtons()
-
-        else:
-            print('Must have at least one if statement!')
     
     #I hope these are correct
     def serialize(self):
@@ -158,19 +85,24 @@ class IfNode(VplNode):
     op_code = OP_CODE_IF
     op_title = "If"
     content_label_objname = "VplNodeIf"
+    TotalOutputs = [1,2]
 
-    def __init__(self, scene, title:str="If"):
+    def __init__(self, scene, title:str="If", outputList=[1,2]):
         self.variablesRef = VariablesData()
-        super().__init__(scene, title, inputs = [1], outputs = [1,2])
+        super().__init__(scene, title, inputs = [1], outputs = outputList)
 
     def initInnerClasses(self):
         self.content = IfNodeContent(self, self.variablesRef)
+        self.TotalIfs = self.content.TotalIfs
         self.grNode = VplGraphicsNode(self)
 
         self.grNode.height = 160
         self.grNode.width = 260
         self.data = NodeData() # THIS FIXES SCOPING ISSUE,
         self.data.nodeType = self.op_code
+
+        
+        self.registerButtons()
 
     def setVariableData(self, variables): # wires up stuff, see subWindow.py
         self.variablesRef = variables
@@ -210,3 +142,91 @@ class IfNode(VplNode):
         self.data.val = -1
         self.data.valType = TYPE_INT
         return
+
+    def registerButtons(self):
+        self.content.subBtn.clicked.connect(self.decreaseWidgetSize)
+        self.content.addBtn.clicked.connect(self.increaseWidgetSize)
+
+    def increaseWidgetSize(self):
+        
+        self.TotalIfs = self.TotalIfs + 1
+
+        self.content.layout.removeWidget(self.content.subBtn)
+        self.content.subBtn.deleteLater()
+        self.content.subBtn = None
+        self.content.layout.removeWidget(self.content.elseLbl)
+        self.content.elseLbl.deleteLater()
+        self.content.elseLbl = None
+        self.content.layout.removeWidget(self.content.addBtn)
+        self.content.addBtn.deleteLater()
+        self.content.addBtn = None      
+
+        self.content.comboBox.append(QComboBox(self.content))
+        self.content.comboBox[self.TotalIfs - 1].addItems(self.content.initialList)
+
+        self.content.edit.append(QLineEdit('' , self.content))
+        self.content.comboBox[self.TotalIfs - 1].setLineEdit(self.content.edit[self.TotalIfs - 1])
+
+        self.content.edit[self.TotalIfs - 1].setAlignment(Qt.AlignCenter)
+        
+        self.content.subBtn = QPushButton('-', self.content)
+        self.content.addBtn = QPushButton('+', self.content)
+        self.content.elseLbl = QLabel('Else', self.content)
+        self.content.elseLbl.setAlignment(Qt.AlignCenter)
+
+        self.content.layout.setRowStretch(self.TotalIfs, 3)
+        self.content.layout.setRowStretch(self.TotalIfs + 1, 3)
+        
+        self.content.layout.addWidget(self.content.comboBox[self.TotalIfs - 1], self.TotalIfs, 1, 1, 3)        
+        
+        self.content.layout.addWidget(self.content.subBtn, self.TotalIfs + 1, 2)
+        self.content.layout.addWidget(self.content.elseLbl, self.TotalIfs + 1, 3)
+        self.content.layout.addWidget(self.content.addBtn, self.TotalIfs + 1, 1)
+        
+        self.TotalOutputs.insert(-2, 1)
+        self.newSockets([1], self.TotalOutputs, True)
+
+        self.grNode.height += 43
+        
+        self.content.redrawComboBox()
+        self.registerButtons()
+
+    def decreaseWidgetSize(self):
+
+        if (self.TotalIfs > 1):
+            self.content.layout.removeWidget(self.content.subBtn)
+            self.content.subBtn.deleteLater()
+            self.content.subBtn = None
+            self.content.layout.removeWidget(self.content.elseLbl)
+            self.content.elseLbl.deleteLater()
+            self.content.elseLbl = None
+            self.content.layout.removeWidget(self.content.addBtn)
+            self.content.addBtn.deleteLater()
+            self.content.addBtn = None   
+            
+            self.content.layout.removeWidget(self.content.comboBox[self.TotalIfs - 1])
+            self.content.comboBox[self.TotalIfs - 1].setParent(None) 
+            self.content.comboBox.pop()
+            self.content.edit.pop()
+
+            self.TotalIfs = self.TotalIfs - 1
+            
+            self.content.subBtn = QPushButton('-', self.content)
+            self.content.addBtn = QPushButton('+', self.content)
+            self.content.elseLbl = QLabel('Else', self.content)
+            self.content.elseLbl.setAlignment(Qt.AlignCenter)
+            
+            self.content.layout.addWidget(self.content.subBtn, self.TotalIfs + 1, 2)
+            self.content.layout.addWidget(self.content.elseLbl, self.TotalIfs + 1, 3)
+            self.content.layout.addWidget(self.content.addBtn, self.TotalIfs + 1, 1)
+            
+            self.TotalOutputs.pop(-2)
+            self.newSockets([1], self.TotalOutputs, False)
+
+            self.grNode.height -= 43
+
+            self.registerButtons()
+
+        else:
+            print('Must have at least one if statement!')
+
