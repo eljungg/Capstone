9f690f1bad33c48532590dea688da1b9c5e6be53
luke.schulz <luke_schulz@gmail.com>
2021-04-21 16:41:13 -0700

    fixed a bug with VPL types not passing correctly and breaking on variable calculate data interaction

--- test/util.py
+++ test/util.py
@@ -23,3 +23,41 @@ def stringToValType(valStr):
         return TYPE_BOOL
     if valStr == "char":
         return TYPE_CHAR
+
+def determineDataType(val):
+        #take any value, return VPL_TYPE
+        if __isInt(val) == True:
+            valType = TYPE_INT
+        elif __isFloat(val) == True:
+            valType = TYPE_DOUBLE
+        elif __isBool(val) == True:
+            valType = TYPE_BOOL
+        elif __isChar(val) == True:
+            valType = TYPE_CHAR
+        else:
+            valType = TYPE_STRING
+        return valType
+
+def __isInt(val): #helper function for determineType
+    try:
+        int(val)
+        return True
+    except ValueError:
+        return False
+def __isFloat(val):
+    try:
+        float(val)
+        return True
+    except ValueError:
+        return False
+def __isBool(val):
+    lcVal = val.lower()
+    if lcVal == "false" or lcVal == "true":
+        return True
+    else:
+        return False
+def __isChar(val): #Python doesnt do Char, but VIPLE does so we just emulate?
+    if len(val) == 1:
+        return True
+    else:
+        return False
